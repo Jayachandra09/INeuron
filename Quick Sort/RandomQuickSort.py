be09628934deffb4c43821b6b71dc3912503d7e5
Jayachandra09 <103034641+Jayachandra09@users.noreply.github.com>
2024-10-08 15:26:09 +0530

    Create RandomQuickSort.py

---
+++ Quick Sort/RandomQuickSort.py
@@ -0,0 +1,39 @@
+import random
+
+def RandomizedPartition(arr, p, q):
+    #Selcting random pivot element index
+    RandomPivotIndex = random.randrange(p,q)
+    #swapping the element with index 0
+    arr[p], arr[RandomPivotIndex] = arr[RandomPivotIndex], arr[p]
+    return Partition(arr, p ,q)
+
+def Partition(arr, p, q):
+    pivot = arr[p]
+    i = p
+    for j in range(i+1, q+1):
+        if arr[j]<pivot:
+            i+=1
+            #Swap between the values of arr[i] and arr[j]
+            arr[i], arr[j] = arr[j], arr[i]
+        #Final swap between arr[i] and arr[p]
+    arr[i], arr[p] = arr[p], arr[i]
+    return i
+
+def QuickSort(arr, p, q):
+    if p<q:
+        # Divide and Conquer Approach
+        #1. Divide
+        #Function calling for the partition part 
+        mid = RandomizedPartition(arr, p, q)
+        #Recursive function call for the left sub tree
+        QuickSort(arr, p, mid-1)
+        #Recursive function call for the right sub tree
+        QuickSort(arr, mid+1, q)
+    return arr
+
+#Driver Code
+arr = [50, 70, 29, 67, 12, 15, 46, 100, 26, 29]
+p = 0
+q = len(arr)-1
+result = QuickSort(arr, p, q)
+print(result)
